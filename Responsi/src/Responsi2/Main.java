9252afbd26fbbf4dfacad0c83a8f9c8aa43e4588
Ihsan Magribi <ihsanmahgribi@gmail.com>
2022-04-02 09:55:02 +0800

    Responsi 107 Perbaikan

--- Responsi/src/Responsi2/Main.java
+++ Responsi/src/Responsi2/Main.java
@@ -13,11 +13,26 @@ public class Main {
     {
         DataBotol.add(new Botol (merk,warna,kapasitas));
     }
+//    void hapusdatabotol(String merk)
+//    {
+//        DataBotol.removeIf(item -> item.Merk == (merk));
+//    }
+//    void ubahdataktm(String merk,String warna,String kapasitas)
+//    {   
+//        int cek = -1;
+//        for(int i=0; i < DataBotol.size(); i++){
+//            if(DataBotol.get(i).Merk == merk){
+//                cek = i;
+//                System.out.println(cek);
+//                DataBotol.set(cek,new Botol (merk,warna,kapasitas));
+//            }
+//        }
+//    }
     void showdatabotol()
     {
         for(Botol data:DataBotol)
         {
-            System.out.println("Merk Botol : "+data.getmerk()+""+",Warna Botol : "+data.getwarna()+""+",Tanggal Lahir : "+data.getkapsitas()+"");
+            System.out.println("Merk Botol : "+data.getmerk()+""+",Warna Botol : "+data.getwarna()+""+",Tanggal Lahir : "+data.getkapasitas()+"");
         }
     }
     public static void main(String[] args) {
@@ -26,7 +41,7 @@ public class Main {
     Main btl = new Main();
     while(coba==1){
         System.out.println("------------------------------------------");
-        System.out.println(" [1] TAMBAH DATA \n [2] SHOW DATA ");
+        System.out.println(" [1] TAMBAH DATA \n [2] SHOW DATA \n [3] HAPUS DATA \n [4] EDIT DATA \n [0] KELUAR");
         System.out.println("------------------------------------------");
         int pilihan;
         System.out.print("Masukkan Pilihan Anda : ");
@@ -49,7 +64,40 @@ public class Main {
         else if (pilihan == 2){
             btl.showdatabotol();
         }
+//        else if(pilihan == 3){
+//            String hapusdata;
+//            System.out.print("Masukkan Merk : ");
+//            hapusdata = inputdata.next();
+//            btl.hapusdatabotol(hapusdata);
+//        }
+//        else if (pilihan == 4){
+//            String Masukkan;
+//            String Merk;
+//            String Warna;
+//            String Kapasitas;
+//            System.out.print("Merk yang ingin diubah : ");
+//            Masukkan = inputdata.next();
+//            System.out.print("Merk Botol      : ");
+//            Merk = inputdata.next();
+//            
+//            System.out.print("Warna Botol     : ");
+//            Warna = inputdata.next();
+//            
+//            System.out.print("Kapasitas Botol : ");
+//            Kapasitas = inputdata.next();
+//            
+//            btl.isidatabotol(Masukkan,Merk,Warna,Kapasitas);
+//        }
+        else if (pilihan == 0){
+            System.out.println("RESPONSI 2 PBO");
+            System.out.println("");
+            break;
+        }
     }
     
+    }
+
+    private void isidatabotol(String Masukkan, String Merk, String Warna, String Kapasitas) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
